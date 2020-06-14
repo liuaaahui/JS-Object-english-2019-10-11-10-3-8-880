b9d4ff42447c59ab047030f91e28dac35742d994
penny <925580783@qq.com>
2020-06-15 00:04:32 +0800

    Create a js document and complete the above requirements.

---
+++ main.js
@@ -0,0 +1,32 @@
+// 1. Program and observe the following requirements:  
+//   - Create a null object and set its variable name as user; 
+//   - Add a name attribute and set its value as John; 
+//   - Add a surname attribute and set its value as Mike; 
+//   - Modify the value of name attribute as Peter; 
+//   - Delete name attribute of user. 
+var user = {};
+user.name = 'John';
+user.surname = 'Mike';
+user.name = 'Peter';
+delete user.name;
+
+// 2. Program and observe the following requirements: calculate how many pieces of fruit there are in the fruit object and the output should be 50. 
+
+// ```
+// var fruit = {
+// apple: 20,
+// pear: 20,
+// peach: 10
+// };
+// // write your code here...
+// ```
+var fruit = {
+apple: 20,
+pear: 20,
+peach: 10
+};
+var total = 0;
+for(let key in fruit){
+    total += fruit[key];
+}
+console.log(total);
